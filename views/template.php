fbf0f8ec61b1383af4ce083b0993c522a47c3315
carlos349 <carlos.gomes349@gmail.com>
2020-05-20 21:47:02 -0400

    first visual changes

--- views/template.php
+++ views/template.php
@@ -4,7 +4,7 @@
 <title>Birding Fest</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="Colo Shop Template">
+<meta name="description" content="Birding Fest">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="views/styles/style.css">
 <link rel="stylesheet" type="text/css" href="views/styles/util.css">
