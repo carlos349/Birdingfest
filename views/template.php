33593b480300b20b5a36755c002d20449a51dc80
CristiansSans <cristiansleonardo@gmail.com>
2020-06-27 12:51:51 -0400

    changes

--- views/template.php
+++ views/template.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-<title>Mediterranean Birding Fest Round</title>
-<meta name="description" content="Ornithology & Birding School">
+<title>Birding Fest</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
